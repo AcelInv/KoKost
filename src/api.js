c68b2bff87b9ce7c9805bb8ec48ec8834f219ba5
AcelInv <mdadityaaaa@gmail.com>
2025-10-21 17:33:14 +0700

    `Update API base URL in api.js`

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:3001/api";
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 export const getKos = () => axios.get(`${API_BASE}/kos`);
 export const getUsers = () => axios.get(`${API_BASE}/users`);
