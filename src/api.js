b3fe49eb177a9dab231d2046926c1b4ee3492307
AcelInv <mdadityaaaa@gmail.com>
2025-10-21 19:19:22 +0700

    `Added import statement for axios and updated API_URL logging`

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
+import axios from "axios";
+
 console.log("🔗 API_URL =", import.meta.env.VITE_API_URL);
 
-import axios from "axios";
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 const api = axios.create({
